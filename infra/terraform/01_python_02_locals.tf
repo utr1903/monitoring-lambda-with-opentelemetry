a66055b2ba3c9c2c9f7dfe6fb894f2a4c64fa779
uturkarslan <ugurturkarslan@gmail.com>
2023-06-25 16:06:35 +0200

    Add delete Lambda

--- infra/terraform/01_python_02_locals.tf
+++ infra/terraform/01_python_02_locals.tf
@@ -23,4 +23,10 @@ locals {
   python_lambda_update_function_name            = "python-lambda-update-otel"
   python_lambda_update_function_source_dir_path = "../../apps/01_python/update"
   python_lambda_update_function_zip_file_path   = "../../tmp/python_lambda_update.zip"
+
+  # Lambda - delete
+  python_lambda_delete_iam_role_name            = "python_lambda_delete_iam_role"
+  python_lambda_delete_function_name            = "python-lambda-delete-otel"
+  python_lambda_delete_function_source_dir_path = "../../apps/01_python/delete"
+  python_lambda_delete_function_zip_file_path   = "../../tmp/python_lambda_delete.zip"
 }
