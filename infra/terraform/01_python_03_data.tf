716ad0bc56215acc937c1a923eca8dde9287a417
uturkarslan <ugurturkarslan@gmail.com>
2023-06-25 15:02:44 +0200

    Add logging to CloudWatch

--- infra/terraform/01_python_03_data.tf
+++ infra/terraform/01_python_03_data.tf
@@ -15,3 +15,18 @@ data "archive_file" "python_lambda_update" {
   source_dir  = local.python_lambda_update_function_source_dir_path
   output_path = local.python_lambda_update_function_zip_file_path
 }
+
+# IAM policy doc for Lambda logging
+data "aws_iam_policy_document" "python_lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
--- infra/terraform/01_python_05_lambda_create.tf
+++ infra/terraform/01_python_05_lambda_create.tf
@@ -2,16 +2,39 @@
 ### Lambda ###
 ##############
 
+# IAM role for Lambda
 resource "aws_iam_role" "python_lambda_create_iam" {
   name               = local.python_lambda_create_iam_role_name
   assume_role_policy = data.aws_iam_policy_document.assume_role_lambda.json
 }
 
+# IAM policy attachment for Lambda to have full S3 access
 resource "aws_iam_role_policy_attachment" "python_lambda_create_s3_full_access" {
   role       = aws_iam_role.python_lambda_create_iam.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
+# Cloudwatch log group for Lambda
+resource "aws_cloudwatch_log_group" "python_lambda_create" {
+  name              = "/aws/lambda/${local.python_lambda_create_function_name}"
+  retention_in_days = 7
+}
+
+# IAM policy for Lambda logging
+resource "aws_iam_policy" "python_lambda_create_logging" {
+  name        = "python_lambda_create_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.python_lambda_logging.json
+}
+
+# IAM policy attachment for Lambda to log to CloudWatch
+resource "aws_iam_role_policy_attachment" "python_lambda_create_logging" {
+  role       = aws_iam_role.python_lambda_create_iam.name
+  policy_arn = aws_iam_policy.python_lambda_create_logging.arn
+}
+
+# Lambda function
 resource "aws_lambda_function" "python_create" {
   filename      = local.python_lambda_create_function_zip_file_path
   function_name = local.python_lambda_create_function_name
@@ -37,4 +60,9 @@ resource "aws_lambda_function" "python_create" {
       S3_BUCKET_NAME                      = aws_s3_bucket.python.id
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.python_lambda_create_logging,
+    aws_cloudwatch_log_group.python_lambda_create,
+  ]
 }
