4719397ec72cdff89e2ccf5a01e0bd0ec4223fe3
uturkarslan <ugurturkarslan@gmail.com>
2023-06-25 16:38:07 +0200

    Add check Lambda

---
+++ apps/01_python/check/lambda_function.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python3
+
+import os
+import boto3
+import json
+import logging
+
+# Reset and init logger
+logger = logging.getLogger()
+if logger.handlers:
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+logging.basicConfig(level=logging.INFO)
+
+client = boto3.client('s3')
+
+def lambda_handler (event, context):
+
+  s3BucketName = event['Records'][0]['s3']['bucket']['name']
+  s3BucketKey = event['Records'][0]['s3']['object']['key']
+
+  logger.info(s3BucketName)
+  logger.info(s3BucketKey)
+
+  item = json.loads(
+    client.get_object(
+      Bucket=s3BucketName,
+      Key=s3BucketKey,
+    )['Body'].read())
+
+  logger.info(item)
+
+  item['isChecked'] = True
+
+  client.put_object(
+    Body=json.dumps(item),
+    Bucket=s3BucketName,
+    Key=s3BucketKey,
+  )
---
+++ infra/terraform/01_python_10_lambda_check.tf
@@ -0,0 +1,74 @@
+##############
+### Lambda ###
+##############
+
+# IAM role for Lambda
+resource "aws_iam_role" "python_lambda_check_iam" {
+  name               = local.python_lambda_check_iam_role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role_lambda.json
+}
+
+# IAM policy attachment for Lambda to have full S3 access
+resource "aws_iam_role_policy_attachment" "python_lambda_check_s3_full_access" {
+  role       = aws_iam_role.python_lambda_check_iam.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# Cloudwatch log group for Lambda
+resource "aws_cloudwatch_log_group" "python_lambda_check" {
+  name              = "/aws/lambda/${local.python_lambda_check_function_name}"
+  retention_in_days = 7
+}
+
+# IAM policy for Lambda logging
+resource "aws_iam_policy" "python_lambda_check_logging" {
+  name        = "python_lambda_check_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.python_lambda_logging.json
+}
+
+# IAM policy attachment for Lambda to log to CloudWatch
+resource "aws_iam_role_policy_attachment" "python_lambda_check_logging" {
+  role       = aws_iam_role.python_lambda_check_iam.name
+  policy_arn = aws_iam_policy.python_lambda_check_logging.arn
+}
+
+# Lambda function
+resource "aws_lambda_function" "python_check" {
+  filename      = local.python_lambda_check_function_zip_file_path
+  function_name = local.python_lambda_check_function_name
+
+  role    = aws_iam_role.python_lambda_check_iam.arn
+  handler = "lambda_function.lambda_handler"
+
+  source_code_hash = data.archive_file.python_lambda_check.output_base64sha256
+
+  runtime = "python3.10"
+  timeout = 10
+
+  layers = [
+    "arn:aws:lambda:${var.AWS_REGION}:901920570463:layer:aws-otel-python-amd64-ver-1-14-0:1"
+  ]
+
+  environment {
+    variables = {
+      AWS_LAMBDA_EXEC_WRAPPER             = "/opt/otel-instrument"
+      OPENTELEMETRY_COLLECTOR_CONFIG_FILE = "/var/task/otel/collector.yaml"
+      NEWRELIC_OTLP_ENDPOINT              = substr(var.NEWRELIC_LICENSE_KEY, 0, 2) == "eu" ? "otlp.eu01.nr-data.net:4317" : "otlp.nr-data.net:4317"
+      NEWRELIC_LICENSE_KEY                = var.NEWRELIC_LICENSE_KEY
+      OUTPUT_S3_BUCKET_NAME                = aws_s3_bucket.python_output.id
+    }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.python_lambda_check_logging,
+    aws_cloudwatch_log_group.python_lambda_check,
+  ]
+}
+
+# Lambda trigger for SQS
+resource "aws_lambda_event_source_mapping" "python_sqs_trigger_for_lambda" {
+  event_source_arn = aws_sqs_queue.python_queue.arn
+  function_name    = aws_lambda_function.python_check.arn
+}
