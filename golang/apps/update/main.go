26b609fc5a4d13ea70dd96103a7bfdcc2789911e
uturkarslan <ugurturkarslan@gmail.com>
2023-07-20 22:49:14 +0200

    Implement S3 get

--- golang/apps/update/main.go
+++ golang/apps/update/main.go
@@ -2,24 +2,31 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda/xrayconfig"
 	"go.opentelemetry.io/contrib/propagators/aws/xray"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var (
-	AWS_REGION           string
-	OTEL_SERVICE_NAME    string
-	INPUT_S3_BUCKET_NAME string
-	uploader             *s3manager.Uploader
+	OTEL_SERVICE_NAME     string
+	OUTPUT_S3_BUCKET_NAME string
+	SQS_QUEUE_URL         string
+	downloader            *s3manager.Downloader
 )
 
 type CustomObject struct {
@@ -31,12 +38,12 @@ type CustomObject struct {
 func main() {
 
 	// Parse environment variables
-	AWS_REGION = os.Getenv("AWS_REGION")
 	OTEL_SERVICE_NAME = os.Getenv("OTEL_SERVICE_NAME")
-	INPUT_S3_BUCKET_NAME = os.Getenv("INPUT_S3_BUCKET_NAME")
+	OUTPUT_S3_BUCKET_NAME = os.Getenv("INPUT_S3_BUCKET_NAME")
+	SQS_QUEUE_URL = os.Getenv("SQS_QUEUE_URL")
 
-	// Create a s3 uploader
-	uploader = s3manager.NewUploader(session.Must(session.NewSession()))
+	// Create a s3 downloader
+	downloader = s3manager.NewDownloader(session.Must(session.NewSession()))
 
 	// Get context
 	ctx := context.Background()
@@ -64,9 +71,105 @@ func main() {
 	lambda.Start(otellambda.InstrumentHandler(handler, xrayconfig.WithRecommendedOptions(tp)...))
 }
 
-func handler(ctx context.Context, s3Event events.S3Event) {
+func handler(
+	ctx context.Context,
+	s3Event events.S3Event,
+) {
+
+	// Loop over all s3 records
 	for _, record := range s3Event.Records {
+
 		s3 := record.S3
 		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+
+		// Start parent span
+		ctx, parentSpan := startParentSpan(ctx, record)
+		defer parentSpan.End()
+
+		// Get the object from input S3
+		objectBytes, err := getObjectFromS3(ctx, parentSpan, record)
+		if err != nil {
+			return
+		}
+
+		customObject := &CustomObject{}
+		json.Unmarshal(objectBytes, customObject)
+
+		fmt.Println("Item: " + customObject.Item)
+		fmt.Println("IsUpdated: " + strconv.FormatBool(customObject.IsUpdated))
+		fmt.Println("IsChecked: " + strconv.FormatBool(customObject.IsChecked))
 	}
+}
+
+func startParentSpan(
+	ctx context.Context,
+	record events.S3EventRecord,
+) (
+	context.Context,
+	trace.Span,
+) {
+	// Create tracer
+	tracer := otel.Tracer(OTEL_SERVICE_NAME)
+
+	// Start parent span
+	return tracer.Start(ctx, "main.handler",
+		trace.WithSpanKind(trace.SpanKindConsumer),
+		trace.WithAttributes([]attribute.KeyValue{
+			semconv.FaaSTriggerDatasource,
+			semconv.FaaSDocumentOperationInsert,
+			semconv.FaaSDocumentTime(record.EventTime.UTC().String()),
+			semconv.FaaSDocumentCollection(record.S3.Bucket.Name),
+			semconv.FaaSDocumentName(record.S3.Object.Key),
+		}...))
+}
+
+func getObjectFromS3(
+	ctx context.Context,
+	parentSpan trace.Span,
+	record events.S3EventRecord,
+) (
+	[]byte,
+	error,
+) {
+
+	// Start S3 put span
+	ctx, s3GetSpan := startS3GetSpan(ctx, parentSpan)
+	defer s3GetSpan.End()
+
+	// Create a writer
+	buff := &aws.WriteAtBuffer{}
+
+	// Get object from input S3
+	_, err := downloader.DownloadWithContext(
+		ctx,
+		buff,
+		&s3.GetObjectInput{
+			Bucket: aws.String(record.S3.Bucket.Name),
+			Key:    aws.String(record.S3.Object.Key),
+		})
+
+	if err != nil {
+		s3GetSpan.SetAttributes([]attribute.KeyValue{
+			semconv.OtelStatusCodeError,
+		}...)
+		return nil, err
+	}
+
+	return buff.Bytes(), err
+}
+
+func startS3GetSpan(
+	ctx context.Context,
+	parentSpan trace.Span,
+) (
+	context.Context,
+	trace.Span,
+) {
+	// Start S3 put span
+	return parentSpan.TracerProvider().Tracer(OTEL_SERVICE_NAME).
+		Start(ctx, "S3.PutObject",
+			trace.WithSpanKind(trace.SpanKindClient),
+			trace.WithAttributes([]attribute.KeyValue{
+				semconv.NetTransportTCP,
+			}...))
 }
