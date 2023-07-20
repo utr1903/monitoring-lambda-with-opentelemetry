8d72c09cd93a54390916e8cfc84b025ee2311a7b
uturkarslan <ugurturkarslan@gmail.com>
2023-07-20 09:09:54 +0200

    Fix go build

--- golang/infra/terraform/02_locals.tf
+++ golang/infra/terraform/02_locals.tf
@@ -14,24 +14,24 @@ locals {
   # Lambda - create
   lambda_create_iam_role_name            = "golang_lambda_create_iam_role"
   lambda_create_function_name            = "golang-lambda-create-otel"
-  lambda_create_function_source_dir_path = "../../apps/create/bin"
+  lambda_create_function_source_dir_path = "../../apps/create"
   lambda_create_function_zip_file_path   = "../../../tmp/golang_lambda_create.zip"
 
   # Lambda - update
   lambda_update_iam_role_name            = "golang_lambda_update_iam_role"
   lambda_update_function_name            = "golang-lambda-update-otel"
-  lambda_update_function_source_dir_path = "../../apps/update/bin"
+  lambda_update_function_source_dir_path = "../../apps/update"
   lambda_update_function_zip_file_path   = "../../../tmp/golang_lambda_update.zip"
 
   # Lambda - delete
   lambda_delete_iam_role_name            = "golang_lambda_delete_iam_role"
   lambda_delete_function_name            = "golang-lambda-delete-otel"
-  lambda_delete_function_source_dir_path = "../../apps/delete/bin"
+  lambda_delete_function_source_dir_path = "../../apps/delete"
   lambda_delete_function_zip_file_path   = "../../../tmp/golang_lambda_delete.zip"
 
   # Lambda - check
   lambda_check_iam_role_name            = "golang_lambda_check_iam_role"
   lambda_check_function_name            = "golang-lambda-check-otel"
-  lambda_check_function_source_dir_path = "../../apps/check/bin"
+  lambda_check_function_source_dir_path = "../../apps/check"
   lambda_check_function_zip_file_path   = "../../../tmp/golang_lambda_check.zip"
 }
